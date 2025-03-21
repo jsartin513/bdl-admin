dfd2253d4f84ab8715d5693ce2b2e0c55c026f02
Jessica Sartin <jessica.sartin@enolink.com>
2025-03-20 20:12:21 -0400

    i think it works

---
+++ app/login/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SignIn from './components/sign-in';
+
+const LoginPage = () => {
+  return (
+    <div>
+      <h1>Login</h1>
+      <SignIn />
+    </div>
+  );
+};
+
+export default LoginPage;
