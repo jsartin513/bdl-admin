7c9453a2aa05c7748bbf896786b3444fb85b0d47
Jessica Sartin <jessica.sartin@enolink.com>
2025-04-01 20:37:28 -0400

    Add session info to debug page

--- app/api/debug/route.ts
+++ app/api/debug/route.ts
@@ -1,5 +1,32 @@
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
+import { auth } from "@/auth"; // Import the `auth` object from your auth.ts file
 
 export async function GET() {
-    return NextResponse.json({ success: true, message: "Backend is working!" });
+
+    // Fetch the session using NextAuth
+    const session  = await auth()
+    // Extract user data from the session
+    const user = session
+        ? {
+            isLoggedIn: true,
+            session: session,
+        }
+        : {
+            isLoggedIn: false,
+            name: null,
+            permissions: [],
+            authTokenExpires: null,
+        };
+
+    // Environment information
+    const appInfo = {
+        nodeEnv: process.env.NODE_ENV || "development", // Replace with other app info as needed
+    };
+
+    return NextResponse.json({
+        success: true,
+        message: "Backend is working!",
+        user,
+        appInfo,
+    });
 }
