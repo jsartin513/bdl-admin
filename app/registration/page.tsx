18eb7f3d742362240ee50ab7e253fcc542b3cc49
Jessica Sartin <jessica.sartin@enolink.com>
2025-05-12 20:29:46 -0400

    update text

--- app/registration/page.tsx
+++ app/registration/page.tsx
@@ -77,6 +77,9 @@ const PaymentPage = async () => {
         <p style={{ margin: "5px 0", fontSize: "1.2em", color: "#004d40" }}>
           Available Spots: {availableSpots} ( {availableWomenSpots} MUST be female)
         </p>
+        <p style={{ margin: "5px 0", fontSize: "1.2em", color: "#004d40" }}>
+          Available spots for non-women: {availableSpots - availableWomenSpots}
+        </p>
       </div>
 
       <h1
